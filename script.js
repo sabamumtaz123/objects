3979b4a1906563c9197656285712e3c7a24e236c
saira mehtab <mehtabsaira26@gmail.com>
2018-07-17 15:32:42 +0500

    msg

---
+++ script.js
@@ -0,0 +1,10 @@
+let car = {
+    "name": "ferrari",
+    "color": "red",
+    "price": "100000",
+}
+console.log("name = " +  car.name);
+console.log("color = " +  car.color);
+console.log("price = " +  car.price);
+
+
